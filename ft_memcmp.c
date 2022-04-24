c4b9861f8bfab2e7c61000b2b41dae362f5e906f
guifgomes47 <guifgomes16@gmail.com>
2022-04-23 23:59:35 -0300

    atoi and memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guilhfer <guilhfer@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/19 18:03:24 by guilhfer          #+#    #+#             */
+/*   Updated: 2022/04/23 14:34:57 by guilhfer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *str1, const void *str2, size_t n)
+{
+	size_t				i;
+	const unsigned char	*p1;
+	const unsigned char	*p2;
+
+	i = 0;
+	p1 = str1;
+	p2 = str2;
+	while (i < n)
+	{
+		if (p1[i] != p2[i])
+		{
+			return (p1[i] - p2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
